d95bd9ede1a9555d30d616eac4a5b531613b3492
PikachuDucvu <ducvubn1@gmail.com>
2023-10-13 02:40:10 +0700

    add project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import About from "@/components/about";
 import Inprogress from "@/components/inprogress";
 import Intro from "@/components/intro";
+import Projects from "@/components/project";
 import SectionDevider from "@/components/sectionDevider";
 import Image from "next/image";
 
@@ -8,7 +10,9 @@ export default function Home() {
     <main className=" flex flex-col items-center px-4">
       <Intro />
       <SectionDevider />
-      <Inprogress />
+      <About />
+      <Projects />
+      {/* <Inprogress /> */}
     </main>
   );
 }
